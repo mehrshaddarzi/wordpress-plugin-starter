06f810d9f559e53264e1eb94ee68abd76d2f0f5a
Mehrshad Darzi <mehrshad198@gmail.com>
2020-12-01 10:52:32 +0330

    Created Table in install Plugin

--- inc/config/install.php
+++ inc/config/install.php
@@ -8,7 +8,28 @@ class install {
 	 * install Plugin Method
 	 */
 	public static function run_install() {
+	global $wpdb;
 
+        // Load DB delta
+        if ( ! function_exists( 'dbDelta' ) ) {
+            require( ABSPATH . 'wp-admin/includes/upgrade.php' );
+        }
+
+        // Charset Collate
+        $collate = $wpdb->get_charset_collate();
+
+        // Create Realty Log Table
+        $create_tbl = ( "
+			CREATE TABLE `{$wpdb->prefix}table_name` (
+				`ID` bigint(200) UNSIGNED NOT NULL AUTO_INCREMENT,
+				`site` varchar(255) NOT NULL,
+				`user_id` bigint(200) NOT NULL,
+				`date` datetime NOT NULL,
+				`type` varchar(50) NOT NULL,
+				`value` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+				PRIMARY KEY  (ID)
+			) {$collate}" );
+        //dbDelta( $create_tbl );
 	}
 
 }
