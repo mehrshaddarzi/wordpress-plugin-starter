83fc25cb77f2e2e99f0775b393ebf5c03d149f99
Mehrshad Darzi <mehrshad198@gmail.com>
2020-12-01 15:22:31 +0330

    add read json Function

--- inc/helper.php
+++ inc/helper.php
@@ -5,7 +5,19 @@ namespace WP_PLUGIN;
  * Class Helper Used in Custom Helper Method For This Plugin
  */
 class Helper {
+  
+    public static function readJson($path)
+    {
+        if (!file_exists($path)) {
+            return array('status' => false, 'message' => 'file not found');
+        }
+        $string = file_get_contents($path);
+        $array = json_decode($string, true);
+        if ($array === null) {
+            return array('status' => false, 'message' => 'problem parse json file');
+        }
 
-
+        return $array;
+    }
 
 }
