a339f3413d61475cdf291b9dcbeba2ea2c853e67
Mehrshad Darzi <mehrshad198@gmail.com>
2021-04-19 12:10:31 +0430

    Update front.php

--- inc/front.php
+++ inc/front.php
@@ -25,6 +25,12 @@ class Front {
 	 * Register Asset
 	 */
 	public function wp_enqueue_style() {
+		
+		// Get Plugin Version
+		$plugin_version = WP_MSTRCLUB::$plugin_version;
+		if (defined('SCRIPT_DEBUG') and SCRIPT_DEBUG === true) {
+		    $plugin_version = time();
+		}
 
 		//Jquery Raty
 		//@see https://github.com/wbotelhos/raty
@@ -36,7 +42,7 @@ class Front {
 		$custom_css = ".test {color: " . WP_PLUGIN::$option['star_color'] . ";}";
 		wp_add_inline_style( self::$asset_name, $custom_css );
 
-		wp_enqueue_script( self::$asset_name, WP_PLUGIN::$plugin_url . '/asset/public/script.js', array( 'jquery' ), WP_PLUGIN::$plugin_version, false );
+		wp_enqueue_script( self::$asset_name, WP_PLUGIN::$plugin_url . '/asset/public/script.js', array( 'jquery' ), $plugin_version, false );
 		wp_localize_script( self::$asset_name, 'wp_reviews_js', array(
 			'ajax'          => home_url() . '/?WP_PLUGIN_check_notification=yes&time=' . current_time( 'timestamp' ),
 			'is_login_user' => ( is_user_logged_in() ? 1 : 0 )
