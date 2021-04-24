3f3b84c5a4bd9a932938e03f1a26b6803e5eeffa
Mehrshad Darzi <mehrshad198@gmail.com>
2021-04-24 08:39:39 +0430

    Update script.js

--- asset/admin/js/script.js
+++ asset/admin/js/script.js
@@ -1,4 +1,27 @@
-//Plugin Admin Area
 jQuery(document).ready(function ($) {
+  // Ajax Url
+  const ajax_url = wp_plugin.ajax;
+  
+  // Example Ajax
+  /*$(document).on('click', '#button', function (e) {
+        e.preventDefault();
+        
+        $.ajax({
+            url: ajax_url,
+            type: 'GET',
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            cache: false,
+            data: {
+             'action': '' 
+            },
+            success: function (data, textStatus, xhr) {
+                $("#tag-id").html(data.html);
+            },
+            error: function (xhr, status, error) {
+                $("#tag-id").html(xhr.responseJSON.html);
+            }
+        });
+    });*/
 
 });
